3c9ceb469cf0c6f3c1fa69fd9ec9df6d95587d9e
OMKARS-SURFACE-\Omkar Dusane <omkardusane@gmail.com>
2019-04-14 12:08:21 +0530

    v 0.0.2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 var f = (keyName, callback) => {
-    const proc = exec('ssh-keygen.exe ' + keyName, {
+    const proc = exec('ssh_keygen.exe ' + keyName, {
         cwd: __dirname + "/bin"
     }, function (error, stdout, stderr) {
         console.log('[EVALD]', { error, stdout, stderr })
--- test/afterInstall.js
+++ test/afterInstall.js
@@ -1,4 +1,4 @@
-let s = require('ssh_keygen_npm@0.0.1');
+let s = require('ssh_keygen_npm');
 s('./outs/yolo', (error, stdout, stderr) => {
     console.log('[TEST EVAL]', { error, stdout, stderr })
 });
